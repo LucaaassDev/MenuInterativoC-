5db671edfcb338b1202a0bf60df76468dce959f8
LucaaassDev <lucasgfranco.dev@gmail.com>
2025-06-19 23:30:18 -0300

    Corrigindo Erros com o Console.Readline

--- Program.cs
+++ Program.cs
@@ -17,40 +17,47 @@ namespace Menu
             Console.WriteLine("===================================");
 
             Console.Write("Escolha o programa:\n 1- Calcular Média\n 2- Par ou Impar\n 3- Calculadora utilizando Switch\n 4- Classificador de Idade \n");
-            int opcao = int.Parse(Console.ReadLine());
+            string? entrada = Console.ReadLine();
 
-            switch (opcao)
+            if (int.TryParse(entrada, out int opcao))
             {
-                case 1:
-                    Console.WriteLine("===================================");
-                    Console.WriteLine("        Calculador de Média        ");
-                    Console.WriteLine("===================================");
-                    menu.aprovadoReprovado();
-                    break;
-                case 2:
-                    Console.WriteLine("===================================");
-                    Console.WriteLine("            Par ou Impar           ");
-                    Console.WriteLine("===================================");
-                    menu.parImpar();
-                    break;
-                case 3:
-                    Console.WriteLine("===================================");
-                    Console.WriteLine("   Calculadora utilizando Switch   ");
-                    Console.WriteLine("===================================");
-                    menu.calculadoraSwitch();
-                    break;
-                case 4:
-                    Console.WriteLine("===================================");
-                    Console.WriteLine("      Classificador de Idade       ");
-                    Console.WriteLine("===================================");
-                    menu.classificadorIdade();
-                    break;
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("===================================");
+                        Console.WriteLine("        Calculador de Média        ");
+                        Console.WriteLine("===================================");
+                        menu.aprovadoReprovado();
+                        break;
+                    case 2:
+                        Console.WriteLine("===================================");
+                        Console.WriteLine("            Par ou Impar           ");
+                        Console.WriteLine("===================================");
+                        menu.parImpar();
+                        break;
+                    case 3:
+                        Console.WriteLine("===================================");
+                        Console.WriteLine("   Calculadora utilizando Switch   ");
+                        Console.WriteLine("===================================");
+                        menu.calculadoraSwitch();
+                        break;
+                    case 4:
+                        Console.WriteLine("===================================");
+                        Console.WriteLine("      Classificador de Idade       ");
+                        Console.WriteLine("===================================");
+                        menu.classificadorIdade();
+                        break;
 
 
 
 
 
 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido");
             }
 
 
